3b67f0734262f0c45216fd03b52e7d3d58b19148
shownola <swasieko@gmail.com>
2019-07-28 14:25:40 -0700

    Initial Commit

---
+++ src/Joke.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Joke extends Component {
+  render(){
+    return(
+
+    )
+  }
+
+
+}
+
+export default Joke;
---
+++ src/JokeList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Joke from './Joke';
+
+class JokeList extends Component {
+  render(){
+    return (
+
+    )
+
+  }
+}
+
+export default JokeList;
