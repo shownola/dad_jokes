da77933b61a024705d45c305266f7b7eaf9cd443
shownola <swasieko@gmail.com>
2019-07-30 12:00:41 -0700

    Updates

---
+++ precache-manifest.e0cd2ae7608faed3d5cc55232a6d40c5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33f3552d27ec3a0a169b272f5dce1697",
+    "url": "/dad_jokes/index.html"
+  },
+  {
+    "revision": "4b5e5b581868c2c0bd00",
+    "url": "/dad_jokes/static/css/main.2d7a7573.chunk.css"
+  },
+  {
+    "revision": "754bf9c7c7e072d7f537",
+    "url": "/dad_jokes/static/js/2.36f8ba4e.chunk.js"
+  },
+  {
+    "revision": "4b5e5b581868c2c0bd00",
+    "url": "/dad_jokes/static/js/main.b386f89a.chunk.js"
+  },
+  {
+    "revision": "9ed4a5a542a04f0d8d01",
+    "url": "/dad_jokes/static/js/runtime~main.a259d400.js"
+  }
+]);
